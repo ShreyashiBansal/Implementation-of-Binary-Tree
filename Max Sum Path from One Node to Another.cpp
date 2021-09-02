4e84578e1a01743da45c51af0f411db9af619143
Shreyashi Bansal <65393257+ShreyashiBansal@users.noreply.github.com>
2021-09-02 21:20:27 +0530

    Create Max Sum Path from One Node to Another.cpp

---
+++ Max Sum Path from One Node to Another.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+int maxPathSumUtil(Node* root, int &ans){
+    if(root == NULL) {
+        return 0;
+    }
+    
+    int left = maxPathSumUtil(root->left, ans);
+    int right = maxPathSumUtil(root->right, ans);
+    
+    int nodeMax = max(max(root->data, root->data + left +  right), 
+                      max(root->data + left, root->data + right));
+    ans = max(ans, nodeMax);
+    
+    int singlepathsum = max(root->data,max(root->data + left, root->data + right));
+    return singlepathsum;
+}
+
+int maxPathSum(Node* root){
+    int ans = INT_MIN;
+    maxPathSumUtil(root, ans);
+    return ans;
+    
+}
+/*
+       1
+      /  \
+     2    3
+    / \  / \
+   4  5  6  7
+*/
+
+int main(){
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left  = new Node(6);
+    root->right->right = new Node(7);
+    
+    cout<<maxPathSum(root);
+    
+    return 0;
+}
+
+//Output: 18
